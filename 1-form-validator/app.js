72efa0f877f3fee2cf077dd8435e53e98422160a
zulhamsy <zulham.sy@gmail.com>
2020-09-14 06:28:59 +0700

    regex matching and validation

--- 1-form-validator/app.js
+++ 1-form-validator/app.js
@@ -4,48 +4,46 @@ const name = document.querySelector('#name'),
   pass = document.querySelector('#pass'),
   pass2 = document.querySelector('#pass2');
 
-// Regex Matching
-const re_name = /^\w+$/gi,
-  re_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-  re_pass = /^[a-z0-9]{6,}$/gi;
-  
 // checking function
 function validName(e) {
-	const input = e.target.value;
-	if(re_name.exec(input) != null) {
-		console.log('valid name');
-	} else {
-		console.log('invalid name');
-	}
+  const input = e.target.value;
+  const re = /^\w+$/gi;
+  if (re.test(input)) {
+    name.classList.remove('is-invalid');
+  } else {
+    name.classList.add('is-invalid');
+  }
 }
 
 function validEmail(e) {
-	const input = e.target.value;
-	if (re_email.exec(input) != null) {
-	  console.log('valid email');
-	} else {
-	  console.log('invalid email');
-	}
+  const input = e.target.value;
+  const re =  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (re.test(input)) {
+    email.classList.remove('is-invalid');
+  } else {
+    email.classList.add('is-invalid');
+  }
 }
 
 function validPass(e) {
-	const input = e.target.value;
-	if (re_pass.exec(input) != null) {
-	  console.log('valid password');
-	} else {
-	  console.log('invalid password');
-	}
+  const input = e.target.value;
+  const re = /^[a-z0-9]{6,}$/gi;
+  if (re.test(input)) {
+    pass.classList.remove('is-invalid');
+  } else {
+    pass.classList.add('is-invalid');
+  }
 }
 
 function confirmPass(e) {
-	const input = e.target.value;
-	if(input === pass.value) {
-		console.log('password match');
-	} else {
-		console.log('password not match');
-	}
+  const input = e.target.value;
+  if (input === pass.value) {
+    pass2.classList.remove('is-invalid');
+  } else {
+    pass2.classList.add('is-invalid');
+  }
 }
-  
+
 // event listener
 name.addEventListener('blur', validName);
 email.addEventListener('blur', validEmail);
