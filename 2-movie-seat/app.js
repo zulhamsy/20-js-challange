14aa48c4e983dd02ffee39fe0e4c5fe78046941f
Zulham Syafrawi <official.zulham@gmail.com>
2020-09-18 04:36:55 +0700

    adding basic functionality - change price and seat

--- 2-movie-seat/app.js
+++ 2-movie-seat/app.js
@@ -3,6 +3,12 @@ const movie = document.querySelector('select');
 const seats = document.querySelectorAll('.seat-select .seat');
 const container = document.querySelector('.seat-select');
 const price = document.querySelector('#price');
+const counter = document.querySelector('#counter');
+
+// function helper
+function __selected() {
+  return document.querySelectorAll('.seat-select .seat-selected');
+}
 
 // function handler
 function selectSeat(e) {
@@ -12,22 +18,30 @@ function selectSeat(e) {
     // check seat state
     if (!target.classList.contains('seat-selected')) {
       target.classList.add('seat-selected');
+      counter.innerText = __selected().length;
+      changePrice();
     } else {
       target.classList.remove('seat-selected');
+      counter.innerText = __selected().length;
+      changePrice();
     }
   }
 }
 
 function selectMovie(e) {
-	const movie = e.target.value;
-	const list = {
-		avenger: 10000,
-		theraid: 15000
-	};
-	price.innerText = list[movie];
+  const movie = e.value;
+  const list = {
+    avenger: 10000,
+    theraid: 15000
+  };
+  return list[movie];
+}
+
+function changePrice(e) {
+  price.innerText = counter.innerText * selectMovie(movie);
 }
 
 // event handler
-movie.addEventListener('change', selectMovie);
 
-container.addEventListener('click', selectSeat)
+container.addEventListener('click', selectSeat);
+movie.addEventListener('change', changePrice);
