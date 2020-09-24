979f0c1aac05edcdf92add2ffcbdc32ce1488b0e
zulhamsy <zulham.sy@gmail.com>
2020-09-24 19:58:26 +0700

    slide menu close from clicking jumbotron

--- 4-menu-slider-popup/app.js
+++ 4-menu-slider-popup/app.js
@@ -2,11 +2,18 @@
 const burger = document.querySelector('.hamburger');
 const slider = document.querySelector('.slider');
 const close = document.querySelector('#close-menu');
+const jumbotron = document.querySelector('.title');
+
+// menu state
+const sliderMenu = {
+	state: false
+};
 
 // event
 function showMenu(e) {
 	// show menu
 	slider.style.transform = 'translateX(0%)';
+	sliderMenu.state = true;
 	// hide hamburger
 	burger.style.transform = 'translateX(100%)';
 }
@@ -14,10 +21,18 @@ function showMenu(e) {
 function closeMenu(e) {
 	// hide menu
 	slider.style.transform = 'translateX(-100%)';
+	sliderMenu.state = false;
 	// show hamburger
 	burger.style.transform = 'translateX(0%)';
+}
+
+function jumbloClose(e) {
+	if(sliderMenu.state) {
+		closeMenu();
+	}
 }
 
 // event listener
 burger.addEventListener('click', showMenu);
 close.addEventListener('click', closeMenu);
+jumbotron.addEventListener('click', jumbloClose);
